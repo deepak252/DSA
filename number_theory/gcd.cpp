220be787f4bde70f1d78bf2b40411715db326fcd
deepak252 <trojanhorsedc@gmail.com>
2023-12-29 17:03:36 +0530

    GCD Iterative

--- number_theory/gcd.cpp
+++ number_theory/gcd.cpp
@@ -6,8 +6,17 @@ int gcd(int a, int b){
   if(b==0) return a;
   return gcd(b,a%b);
 }
+//Iterative
+int gcd2(int a, int b){
+  while(b!=0){
+    int temp = b;
+    b = a%b;
+    a = temp;
+  }
+  return a;
+}
 
 int main(){
-  cout<<gcd(12,18)<<endl;
+  cout<<gcd2(12,18)<<endl;
   return 0;
 }
