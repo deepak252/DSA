76949535d4f4f16965341c5e0f202c4a0d9e872f
deepak252 <deepakkudc16102000@gmail.com>
2025-04-16 00:26:26 +0530

    Fenwick tree fix

--- binary_indexed_tree/fenwick_tree.cpp
+++ binary_indexed_tree/fenwick_tree.cpp
@@ -2,19 +2,61 @@
 
 using namespace std;
 
+// class FenwickTree{
+// private: 
+//     int n;
+//     vector<int> arr;
+// public:
+//     FenwickTree(int size){
+//         this->n = size+1;
+//         arr.resize(size+1);
+//     }
+
+//     void add(int i, int num){
+//         if(i<=0 || i>=n) return;
+//         while(i<n){
+//             arr[i]+=num;
+//             i=i+(i&(-i));
+//         }
+//     }
+
+//     // Get sum from index 0 to i
+//     int sum(int i){
+//         if(i>=n || i<0) return -1;
+//         int sum = 0;
+//         while(i>0){
+//             sum+=arr[i];
+//             i=i-(i&(-i));
+//         }
+//         return sum;
+//     }
+
+//     void show(){
+//         for(auto num: arr) cout<<num<<" ";
+//         cout<<endl;
+//     }
+
+//     void showPrefixSum(){
+//         for(int i=1; i<n; i++){
+//             cout<<this->sum(i)<<" ";
+//         }
+//         cout<<endl;
+//     }
+// };
+
 class FenwickTree{
 private: 
     int n;
+    // 1-index arr
     vector<int> arr;
 public:
     FenwickTree(int size){
-        this->n = size+1;
+        this->n = size;
         arr.resize(size+1);
     }
 
     void add(int i, int num){
-        if(i<=0 || i>=n) return;
-        while(i<n){
+        while(i<=n){
             arr[i]+=num;
             i=i+(i&(-i));
         }
@@ -22,7 +64,6 @@ public:
 
     // Get sum from index 0 to i
     int sum(int i){
-        if(i>=n || i<0) return -1;
         int sum = 0;
         while(i>0){
             sum+=arr[i];
@@ -37,7 +78,7 @@ public:
     }
 
     void showPrefixSum(){
-        for(int i=1; i<n; i++){
+        for(int i=1; i<=n; i++){
             cout<<this->sum(i)<<" ";
         }
         cout<<endl;
@@ -45,12 +86,19 @@ public:
 };
 
 int main() {
-    int n = 12;
+    int n = 7;
     FenwickTree ft = FenwickTree(n);
+    // 1-indexed
     ft.add(6, 4);
     ft.showPrefixSum();
     ft.add(3, 5);
     ft.showPrefixSum();
     ft.add(3, 5);
     ft.showPrefixSum();
+    ft.add(1, 2);
+    ft.showPrefixSum();
+    ft.add(7, 3);
+    ft.showPrefixSum();
+
+    return 0;
 }
